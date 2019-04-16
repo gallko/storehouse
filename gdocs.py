c3d49d332972c62b25c279f6f742a06724ff8888
rus <rru@npotelecom.ru>
2019-04-16 16:26:22 +0400

    tests - ok

--- gdocs.py
+++ gdocs.py
@@ -1,4 +1,13 @@
+from builtins import print
+
 from departs import get_obj
+
+
+def get_index_field(store: list, name: str):
+    try:
+        return store.index(name)
+    except ValueError:
+        return None
 
 
 class Gdocs:
@@ -8,6 +17,7 @@ class Gdocs:
         self.cond = condition
         self.repl = replace
         self.obj111 = None
+        self.count = 0
 
     def update(self):
         body = {'Input': [
@@ -20,7 +30,7 @@ class Gdocs:
                 'head': '106#10',
                 'original': ['1'],
                 'values': [[self.cond[0]]]
-             }
+            }
         ]}
         docs = self.req.send('GDocs', body)
         if docs['errorCode'] != 0:
@@ -39,55 +49,78 @@ class Gdocs:
             print('Отменено пользователем')
             exit(0)
         values = self.obj111['values']
-        doc_worker = DocWorker(self.req)
+        original = self.obj111['original']
+
+        doc_worker = DocWorker(self.req, self.cond, self.repl)
+        index_type = get_index_field(original, '5')
+        index_id = get_index_field(original, '1')
         for i in range(len(values[0])):
-            doc_worker.dispatch(values[2][i], values[0][i])
-            input('next')
+            ok = doc_worker.process(values[index_type][i], values[index_id][i])
+            if ok:
+                self.count = self.count + 1
+        print('Обновлено записей: ' + str(self.count))
 
 
 class DocWorker:
 
-    def __init__(self, request) -> None:
+    def __init__(self, request, cond, replace) -> None:
         super().__init__()
-        self.req = request
-
-    def dispatch(self, value, index):
-        method_name = 'process_doc' + str(value)
-        method = getattr(self, method_name)
-        method(index)
+        self.request = request
+        self.cond = cond
+        self.rep = replace
 
     def getdoc(self, proc, index):
         body = {'Input': [{'head': '111', 'original': ['1'], 'values': [[index]]}]}
-        doc = self.req.send(proc, body)
+        doc = self.request.send(proc, body)
         if doc['errorCode'] == 1:
             return None
         return get_obj(doc['shTable'], '111')
 
-    def process_doc0(self, index):
-        doc = self.getdoc('GDoc0', index)
-        print(doc)
-        print('process_doc0 - ' + str(index))
+    def replace(self, obj111: dict) -> dict:
+        #  RID         Name        Flag         RID KPP          Name KPP
+        # '105\\1',   '105\\3',   '105\\31',   '105\\114\\1',   '105\\114\\3'
+        # '105#1\\1', '105#1\\3', '105#1\\31', '105#1\\114\\1', '105#1\\114\\3'
+        f = False
+        names_fields = obj111['original']
+        values = obj111['values']
+        i_rid = get_index_field(names_fields, '105\\1')
+        i_kpp = get_index_field(names_fields, '105\\114\\1')
+        if i_rid is not None and i_kpp is not None and \
+                values[i_rid][0] == self.cond[0] and \
+                values[i_kpp][0] == self.cond[1]:
+            values[i_rid] = [self.rep[0]]
+            values[i_kpp] = [self.rep[1]]
+            f = True
 
-    def process_doc1(self, index):
-        print('process_doc1 - ' + str(index))
+        i_rid = get_index_field(names_fields, '105#1\\1')
+        i_kpp = get_index_field(names_fields, '105#1\\114\\1')
+        if i_rid is not None and i_kpp is not None and \
+                values[i_rid][0] == self.cond[0] and \
+                values[i_kpp][0] == self.cond[1]:
+            values[i_rid] = [self.rep[0]]
+            values[i_kpp] = [self.rep[1]]
+            f = True
 
-    def process_doc4(self, index):
-        print('process_doc4 - ' + str(index))
+        if not f:
+            return {}
+        obj111.pop('fields', None)
+        # obj111.update(values=values)
+        return obj111
 
-    def process_doc5(self, index):
-        print('process_doc5 - ' + str(index))
+    def write_obj(self, proc_name, obj):
+        body = {'Input': [obj]}
+        return self.request.send(proc_name, body)
 
-    def process_doc8(self, index):
-        print('process_doc8 - ' + str(index))
-
-    def process_doc10(self, index):
-        print('process_doc10 - ' + str(index))
-
-    def process_doc11(self, index):
-        print('process_doc11 - ' + str(index))
-
-    def process_doc12(self, index):
-        print('process_doc12 - ' + str(index))
-
-    def process_doc13(self, index):
-        print('process_doc13 - ' + str(index))
+    def process(self, type_doc, index) -> bool:
+        if type_doc in [1, 10, 13]:
+            return False
+        obj111 = self.getdoc('GDoc' + str(type_doc), index)
+        new_obj111 = self.replace(obj111)
+        if not new_obj111:
+            return False
+        result = self.write_obj('UpdGDoc' + str(type_doc), new_obj111)
+        if result['errorCode'] != 0:
+            print('write GDoc' + str(type_doc) + ' id:' + str(index) + ' запись не удачна')
+            print(result)
+            return False
+        return True
--- main.py
+++ main.py
@@ -7,12 +7,43 @@ from gdocs import Gdocs
 
 class Request:
 
-    def __init__(self, addr, port=80, url='/',  login='admin', password='admin'):
+    def __init__(self, addr, port, url,  login, password):
         self.address = addr
         self.login = login
         self.password = password
         self.port = port
         self.url = url
+
+    def check(self):
+        if self.address is None:
+            self.address = input('Виведите адрес сервере (в формате test.ru или IP адрес): ')
+        if self.port is None:
+            port = input('Виведите номер порта: ')
+            try:
+                self.port = int(port)
+            except ValueError:
+                print('Введен не верный номер порта.')
+                exit(-1)
+        if self.login is None:
+            self.login = input('UserName: ')
+        if self.password is None:
+            self.password = input('Password: ')
+        body = {'UserName': self.login, 'Password': self.password}
+        headers = {"Content-type": "application/json"}
+        conn = http.client.HTTPConnection(self.address, self.port, timeout=10)
+        try:
+            conn.request('POST', '/api/sh5info', json.dumps(body), headers)
+        except ValueError:
+            print('Сервер не доступен')
+            conn.close()
+            exit(-1)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        data = json.loads(data.decode('utf-8'))
+        if data['errorCode'] == 1:
+            print(data['errMessage'])
+            exit(-1)
 
     def send(self, proc_name, request={}):
         body = {'UserName': self.login, 'Password': self.password, 'procName': proc_name}
@@ -25,7 +56,6 @@ class Request:
             return None
         data = response.read()
         conn.close()
-        # print(data.decode('utf8'))
         data = data.decode('utf-8')
         return json.loads(data)
 
@@ -40,18 +70,20 @@ class KppDeparts:
 if __name__ == '__main__':
     parser = ArgumentParser(description='Connection to storehouse')
 
-    parser.add_argument('-s', '--server', required=True, type=str,
+    parser.add_argument('-s', '--server', type=str, default=None,
                         dest='server', help='address of server for connection')
-    parser.add_argument('-o', '--port', type=int, default=80,
+    parser.add_argument('-o', '--port', type=int, default=None,
                         dest='port', help='port of server for connection')
 
-    parser.add_argument('-l', '--login', required=True, type=str,
+    parser.add_argument('-l', '--login', type=str, default=None,
                         dest='login', help='username for connection to server')
-    parser.add_argument('-p', '--password', required=True, type=str,
+    parser.add_argument('-p', '--password', type=str, default=None,
                         dest='password', help='password for connection to server')
 
     args = parser.parse_args()
     req = Request(args.server, args.port, '/api/sh5exec', args.login, args.password)
+    req.check()
+
     departs = Departs(req)
     departs.update()
     print('Выбор критерий поиска:')
